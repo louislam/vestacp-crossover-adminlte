3721d696574a9a6d6494f86f60013ec5c408ab34
Louis Lam <louislam@live.com>
2015-03-06 21:30:07 +0800

    working

---
+++ templates/admin/content_header.php
@@ -0,0 +1,8 @@
+<section class="content-header">
+	<h1><?php echo $TAB; ?></h1>
+	<ol class="breadcrumb">
+		<li><a href="/"><i class="fa fa-dashboard"></i> Home</a></li>
+		<li class="active"><?php echo $TAB; ?></li>
+	</ol>
+</section>
+
