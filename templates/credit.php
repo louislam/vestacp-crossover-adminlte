ff7b618d0f29e6e5d6e332a4e6b0b6d3f22800a4
Louis Lam <louislam@live.com>
2015-03-06 20:13:25 +0800

    finish menu

---
+++ templates/credit.php
@@ -0,0 +1,5 @@
+<div style="text-align: center">
+	Developed by <a target="_blank" class="vestacp" href="http://vestacp.com" >Vestacp.com</a><br />
+	Theme by <a target="_blank" class="vestacp" href="http://almsaeedstudio.com/AdminLTE/" >AdminLTE</a>
+Crossover by <a target="_blank" class="vestacp" href="https://louislam.net" >Louis Lam</a>
+</div>
