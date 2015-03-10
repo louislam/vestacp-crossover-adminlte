33127029c1d596d705b7305f93ad9ec32ca5ee8b
Louis Lam <louislam@live.com>
2015-03-10 18:57:43 +0800

    override the default error dialog with better one

--- web/templates/admin/content_header.php
+++ web/templates/admin/content_header.php
@@ -6,5 +6,42 @@
 	</ol>
 </section>
 
+
+
+<!-- Replace display_error_block() with this Modal -->
+<? if (!empty($_SESSION['error_msg'])) : ?>
+	<div class="modal modal-warning" id="error-msg-modal">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title"><?php print __('Error');?></h4>
+				</div>
+				<div class="modal-body">
+					<p></p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-outline pull-right" data-dismiss="modal"><?php echo __('OK') ?></button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script>
+		$(document).ready(function () {
+			$("#error-msg-modal .modal-body").html($("#dialog-message").html());
+			$("#error-msg-modal").modal("show");
+			$(".ui-dialog").remove();
+		});
+	</script>
+<? endif; ?>
+
 <? display_error_block(); ?>
+
+<script>
+	// Destroy the default error dialog
+	$(document).ready(function () {
+		$( "#dialog-message" ).dialog("destroy");
+	});
+</script>
 
