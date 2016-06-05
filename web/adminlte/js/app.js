0e2efe1c8b843393a2162dc43dd7bc9482506692
Louis Lam <louislam@live.com>
2016-06-05 23:31:54 +0800

    update menu

--- web/adminlte/js/app.js
+++ web/adminlte/js/app.js
@@ -14,14 +14,50 @@ var LouisAdminLTE = (function () {
         body.wrapInner('<div class="content-wrapper" />');
         body.wrapInner('<div class="wrapper" />');
         wrapper = $(".wrapper");
-        $.get("/adminlte/view/header.php", function (html) {
-            wrapper.prepend(html);
+        // Load Template
+        $.get("/adminlte/view/template.php", function (html) {
+            body.show();
+            var template = $(html);
+            var sidebar = template.find(".main-sidebar");
+            var sidebarMenu = sidebar.find(".sidebar-menu");
+            var header = template.find(".main-header");
+            wrapper.prepend(header);
+            wrapper.prepend(sidebar);
+            header.find(".username").text($(".l-profile__username").text());
+            header.find(".btn-profile").attr("href", $(".l-profile__username").attr("href"));
+            header.find(".btn-logout").attr("href", $(".l-profile__logout").attr("href"));
+            // Menu items
+            $(".l-stat .l-stat__col").each(function () {
+                var name = LouisAdminLTE.ucFirst($(this).find(".l-stat__col-title").text());
+                var url = $(this).find("a").attr("href");
+                var number = LouisAdminLTE.ucFirst($(this).find("ul li:first-child").text());
+                var item = $("<li><a href=''><i class='fa fa-th'></i> <span></span><small class='label pull-right  bg-green'>new</small></li>");
+                if ($(this).hasClass("l-stat__col--active")) {
+                    item.addClass("active");
+                }
+                item.find("span").text(name);
+                item.find("a").attr("href", url);
+                item.find(".label").text(number);
+                sidebarMenu.append(item);
+            });
+            // Admin Menu
+            sidebarMenu.append('<li class="treeview active"><a href="#"><i class="fa fa-files-o"></i><span>Admin</span></a><ul class="treeview-menu admin-menu"></ul></li>');
+            $(".l-menu__item a").each(function () {
+                var item = $("<li><a><i class=\"fa fa-circle-o\"></i></a></li>");
+                var a = item.find("a");
+                a.attr("href", $(this).attr("href"));
+                a.append($(this).text());
+                $(".admin-menu").append(item);
+            });
+            $(".l-stat, .l-header").hide();
         });
+    };
+    LouisAdminLTE.ucFirst = function (str) {
+        return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
     };
     return LouisAdminLTE;
 }());
 $.ajaxSetup({
-    // Disable caching of AJAX responses
     cache: false
 });
 var louisAdminLTE = null;
--- web/adminlte/js/app.ts
+++ web/adminlte/js/app.ts
@@ -4,6 +4,7 @@
 /// <reference path="jquery.d.ts" />
 "use strict";
 
+
 class LouisAdminLTE {
 
     constructor() {
@@ -22,16 +23,66 @@ class LouisAdminLTE {
         body.wrapInner('<div class="wrapper" />');
         wrapper = $(".wrapper");
 
-        $.get("/adminlte/view/header.php", function (html) {
-            wrapper.prepend(html);
+        // Load Template
+        $.get("/adminlte/view/template.php", function (html) {
+            body.show();
+            var template = $(html);
+            var sidebar = template.find(".main-sidebar");
+            var sidebarMenu = sidebar.find(".sidebar-menu");
+            var header = template.find(".main-header");
+
+            wrapper.prepend(header);
+            wrapper.prepend(sidebar);
+
+            header.find(".username").text($(".l-profile__username").text());
+            header.find(".btn-profile").attr("href", $(".l-profile__username").attr("href"));
+            header.find(".btn-logout").attr("href", $(".l-profile__logout").attr("href"));
+
+            // Menu items
+            $(".l-stat .l-stat__col").each(function () {
+                var name = LouisAdminLTE.ucFirst($(this).find(".l-stat__col-title").text());
+                var url = $(this).find("a").attr("href");
+                var number = LouisAdminLTE.ucFirst($(this).find("ul li:first-child").text());
+
+                var item = $("<li><a href=''><i class='fa fa-th'></i> <span></span><small class='label pull-right  bg-green'>new</small></li>");
+
+                if ($(this).hasClass("l-stat__col--active")) {
+                    item.addClass("active");
+                }
+
+                item.find("span").text(name);
+                item.find("a").attr("href", url);
+                item.find(".label").text(number);
+
+                sidebarMenu.append(item);
+            });
+
+            // Admin Menu
+            sidebarMenu.append('<li class="treeview active"><a href="#"><i class="fa fa-files-o"></i><span>Admin</span></a><ul class="treeview-menu admin-menu"></ul></li>');
+
+            $(".l-menu__item a").each(function () {
+                var item = $("<li><a><i class=\"fa fa-circle-o\"></i></a></li>");
+                var a = item.find("a");
+                a.attr("href", $(this).attr("href"));
+                a.append($(this).text());
+
+                $(".admin-menu").append(item);
+            });
+
+
+            $(".l-stat, .l-header").hide(); 
+
         });
 
 
     }
+
+    public static ucFirst(str : string) {
+        return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    }
 }
 
 $.ajaxSetup ({
-    // Disable caching of AJAX responses
     cache: false
 });
 
