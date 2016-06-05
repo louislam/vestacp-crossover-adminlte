5f2be8297c7b3a7f6630f70f1515a5cb17c1d75a
Louis Lam <louislam@live.com>
2016-06-05 18:23:47 +0800

    update

--- web/adminlte/js/app.js
+++ web/adminlte/js/app.js
@@ -1,9 +1,32 @@
 /**
  * Created by Louis on 5/6/2016.
  */
+/// <reference path="jquery.d.ts" />
+"use strict";
 var LouisAdminLTE = (function () {
     function LouisAdminLTE() {
     }
+    LouisAdminLTE.prototype.initUI = function () {
+        var body = $("body");
+        var wrapper;
+        body.addClass("skin-blue sidebar-mini");
+        // Wrap All Content first
+        body.wrapInner('<div class="content-wrapper" />');
+        body.wrapInner('<div class="wrapper" />');
+        wrapper = $(".wrapper");
+        $.get("/adminlte/view/header.php", function (html) {
+            wrapper.prepend(html);
+        });
+    };
     return LouisAdminLTE;
 }());
+$.ajaxSetup({
+    // Disable caching of AJAX responses
+    cache: false
+});
+var louisAdminLTE = null;
+$(document).ready(function () {
+    louisAdminLTE = new LouisAdminLTE();
+    louisAdminLTE.initUI();
+});
 //# sourceMappingURL=app.js.map
--- web/adminlte/js/app.ts
+++ web/adminlte/js/app.ts
@@ -1,7 +1,43 @@
 /**
  * Created by Louis on 5/6/2016.
  */
+/// <reference path="jquery.d.ts" />
+"use strict";
 
 class LouisAdminLTE {
 
-} 
+    constructor() {
+
+    }
+
+    private initUI() {
+        var body = $("body");
+        var wrapper;
+
+        body.addClass("skin-blue sidebar-mini");
+
+        // Wrap All Content first
+        body.wrapInner('<div class="content-wrapper" />');
+ 
+        body.wrapInner('<div class="wrapper" />');
+        wrapper = $(".wrapper");
+
+        $.get("/adminlte/view/header.php", function (html) {
+            wrapper.prepend(html);
+        });
+
+
+    }
+}
+
+$.ajaxSetup ({
+    // Disable caching of AJAX responses
+    cache: false
+});
+
+var louisAdminLTE = null;
+
+$(document).ready(function () {
+    louisAdminLTE = new LouisAdminLTE();
+    louisAdminLTE.initUI();
+});
